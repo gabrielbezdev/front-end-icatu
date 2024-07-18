df797d9a67e0ef4f3aec76a3c040894ed174c23b
gabrielbezdev <gabrielbvl2024@gmail.com>
2024-07-18 17:03:48 -0300

    feat: project creation

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import LogoIcatu from './assets/LogoIcatu.jpg'
+import "./App.css";
+
+
+function App() {
+  return (
+    <>
+    <div className='logoIcatu'>
+     <img src={LogoIcatu} alt="LogoIcatu" />
+     <h1>Aguardem...</h1>
+     </div>
+    </>
+  );
+}
+
+export default App;
